b55138f95522bfd3d926a7c3979417bf0340419c
justin2707 <39796875+justin2707@users.noreply.github.com>
2024-07-04 00:02:56 -0500

    Create sortedArraytoBST.py
    This python program converts a given sorted Array/LinkedList to a BinarySearchTree.

---
+++ sortedArraytoBST.py
@@ -0,0 +1,15 @@
+
+def sortedArraytoBST(self,nums:list[int])-> TreeNode
+
+        def helper(l,r):
+            if(l>r):
+              return None
+
+            m=(l+r)//2
+            root=TreeNode(nums[m])
+            root.left=helper(l,m-1)
+            root.right=helper(m+1,r)
+            return root
+
+        return helper(0,len(nums)-1)
+            
